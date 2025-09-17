175b42257b18383da201e76e65b75ca460024358
mikkelsen1337 <macztwitch@gmail.com>
2025-09-17 13:55:47 +0200

    fix

--- src/main/java/org/example/FileClient.java
+++ src/main/java/org/example/FileClient.java
@@ -1,4 +1,30 @@
 package org.example;
+import java.io.*;
+import java.net.*;
 
 public class FileClient {
+    public static void main(String[] args) {
+        String serverAdress = "localhost";
+        int port = 5000;
+        String savePath = "modtaget.txt";
+
+        try(Socket socket = new Socket(serverAdress, port)) {
+            System.out.println("Forbundet til serveren...");
+
+            InputStream in = socket.getInputStream();
+            FileOutputStream fos = new FileOutputStream(savePath);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while((bytesRead = in.read(buffer)) != -1) {
+                bos.write(buffer, 0, bytesRead);
+            }
+            System.out.println("Fil modtaget og gemt: " + savePath);
+
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/FileServer.java
+++ src/main/java/org/example/FileServer.java
@@ -1,4 +1,36 @@
 package org.example;
+import java.io.*;
+import java.net.*;
 
-public class FileServer {
+
+public class FileServer{
+    public static void main(String[] args) {
+    int port = 5000;
+    String filePath = "test.txt";
+
+    try(ServerSocket serverSocket = new ServerSocket(port)) {
+        System.out.println("Server venter på forbindelse... ");
+
+        Socket socket = serverSocket.accept();
+        System.out.println("Klient forbundet!");
+
+        File file = new File(filePath);
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+
+        OutputStream out = socket.getOutputStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while((bytesRead = bis.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
+        }
+        System.out.println("Fil sendt: " + filePath);
+
+        bis.close();
+        socket.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    }
+
 }
